228b0ac685cca60eca8718262249d6e1ba038302
Danh Tran <danh.tt1294@gmail.com>
2021-06-29 12:27:36 +0700

    add table

--- components/layout/SideNav.tsx
+++ components/layout/SideNav.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from "react";
 import { Recording16 } from "@carbon/icons-react";
+import { useRouter } from "next/router";
 import {
   SideNav as BXSideNav,
   SideNavItems,
@@ -9,6 +10,8 @@ import {
 
 export const SideNav: FC = ({ children }) => {
   const [open, setOpen] = useState(false);
+
+  const router = useRouter();
 
   return (
     <BXSideNav
@@ -26,16 +29,30 @@ export const SideNav: FC = ({ children }) => {
           title="Cổng Vào - 1"
           large
         >
-          <SideNavMenuItem aria-current="page" href="#">
+          <SideNavMenuItem
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/");
+            }}
+            href="/"
+          >
             Kiểm soát cổng
           </SideNavMenuItem>
-          <SideNavMenuItem href="#">Lịch sử (Sắp có)</SideNavMenuItem>
+          <SideNavMenuItem
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/history");
+            }}
+            href="/history"
+          >
+            Lịch sử
+          </SideNavMenuItem>
         </SideNavMenu>
-        <SideNavMenu renderIcon={Recording16} title="Cổng Ra - 1" large>
-          {/* <SideNavMenuItem href="#">Phân giải 360</SideNavMenuItem>
+        {/* <SideNavMenu renderIcon={Recording16} title="Cổng Ra - 1" large> */}
+        {/* <SideNavMenuItem href="#">Phân giải 360</SideNavMenuItem>
           <SideNavMenuItem href="#">Phân giải 720</SideNavMenuItem>
           <SideNavMenuItem href="#">Phân giải 1080</SideNavMenuItem> */}
-        </SideNavMenu>
+        {/* </SideNavMenu> */}
       </SideNavItems>
     </BXSideNav>
   );
--- pages/history.tsx
+++ pages/history.tsx
@@ -1,5 +1,14 @@
 import { css } from "linaria";
-import { useEffect, useState } from "react";
+import {
+  DataTable,
+  Table,
+  TableHead,
+  TableRow,
+  TableHeader,
+  TableBody,
+  TableCell,
+} from "carbon-components-react";
+import { useEffect, useState, useMemo } from "react";
 import { sort, descend, prop } from "ramda";
 
 import { useSelector, useDispatch } from "@/store/hooks";
@@ -15,7 +24,88 @@ export const History = () => {
     dispatch(doListTracking());
   }, []);
 
-  return <Page>{JSON.stringify(trackings)}</Page>;
+  const headers = useMemo(
+    () => [
+      {
+        key: "imageurl",
+        header: "Hình ảnh",
+      },
+      {
+        key: "id",
+        header: "ID",
+      },
+      {
+        key: "containerid",
+        header: "Số container",
+      },
+      {
+        key: "createdat",
+        header: "Ngày lưu",
+      },
+    ],
+    []
+  );
+
+  return (
+    <Page>
+      <DataTable rows={trackings as any} headers={headers}>
+        {({ rows, headers, getTableProps, getHeaderProps, getRowProps }) => (
+          <Table {...getTableProps()}>
+            <TableHead>
+              <TableRow>
+                {headers.map((header) => (
+                  <TableHeader {...getHeaderProps({ header })}>
+                    {header.header}
+                  </TableHeader>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow {...getRowProps({ row })}>
+                  {row.cells.map((cell) => {
+                    const [idx, field] = cell.id.split(":");
+                    console.log(field);
+
+                    if (field === "imageurl") {
+                      if (!cell.value) {
+                        return (
+                          <TableCell key={cell.id}>Không có hình</TableCell>
+                        );
+                      }
+
+                      return (
+                        <TableCell key={cell.id}>
+                          <img
+                            className="w-full h-28 object-contain object-top"
+                            src={
+                              process.env.NEXT_PUBLIC_MINIO +
+                              cell.value.split("/").slice(4).join("/")
+                            }
+                            alt=""
+                          />
+                        </TableCell>
+                      );
+                    }
+
+                    if (field === "createdat") {
+                      return (
+                        <TableCell key={cell.id}>
+                          {new Date(cell.value * 1000).toUTCString()}
+                        </TableCell>
+                      );
+                    }
+
+                    return <TableCell key={cell.id}>{cell.value}</TableCell>;
+                  })}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </DataTable>
+    </Page>
+  );
 };
 
 export default History;
