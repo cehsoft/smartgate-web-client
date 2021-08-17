f0e678523840dabdaf73f37253a16c386450783c
Danh Tran <danh.tt1294@gmail.com>
2021-08-17 12:54:50 +0700

    remake login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,10 @@
-import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
-
+import { TextInput, Button, Form, FormGroup } from "carbon-components-react";
 import { useForm } from "react-hook-form";
-import { queryInitialing } from "@/libs/server";
+import { css } from "linaria";
 
-import { loadStore } from "@/store/index";
 import { useSelector, useDispatch } from "@/store/hooks";
-import { updateUserInfo, doLogin } from "@/store/slices/session";
-import { Loads } from "@/store/slices/load";
+import { doLogin } from "@/store/slices/session";
 
-import { Page } from "@/components/layout/Page";
-import { Container } from "@/components/Container";
-import { Section } from "@/components/Section";
-import { Button } from "@/components/Button";
-import { Input } from "@/components/Input";
 import { useGuest } from "@/libs/hooks";
 
 export const Login = () => {
@@ -34,55 +25,63 @@ export const Login = () => {
   };
 
   return (
-    <Container className="mt-8">
-      <Section>
-        <form
+    <div className={["w-full flex flex-row", styleFullHeight] as any}>
+      <div className={["w-full lg:w-1/4 p-4 h-full", styleBgLoginPanel] as any}>
+        <Form
           onSubmit={(e) => {
             e.preventDefault();
             handleSubmit(handleFormSubmit)();
           }}
         >
-          <Input
-            {...register("username", { required: true })}
-            placeholder="Tên đăng nhập"
-          ></Input>
-          <Input
-            {...register("password", { required: true })}
-            className="mt-2"
-            type="password"
-            placeholder="Mật khẩu"
-          ></Input>
-          <Button className="mt-2" type="submit" kind="primary" size="small">
-            Đăng nhập
-          </Button>
-        </form>
-      </Section>
-      <Section>
-        <p>{error}</p>
-      </Section>
-    </Container>
+          <FormGroup legendText="">
+            <TextInput
+              className="mb-4"
+              id="username"
+              labelText="Tên đăng nhập"
+              placeholder="thinhnguyen@spitc.vn"
+              {...register("username", { required: true })}
+            ></TextInput>
+            <TextInput.PasswordInput
+              id="password"
+              labelText="Mật khẩu"
+              placeholder="password"
+              {...register("password", { required: true })}
+            ></TextInput.PasswordInput>
+          </FormGroup>
+          <div className="float-right">
+            <Button size="field" type="submit">
+              Đăng nhập
+            </Button>
+          </div>
+          <div className="clear-both"></div>
+        </Form>
+      </div>
+      <div className="hidden  w-3/4 lg:flex flex-row justify-center items-center">
+        <img className={styleBgEmptyPanel} src="/imgs/login-bg-2.png" alt="" />
+      </div>
+    </div>
   );
 };
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   const { initialProps } = queryInitialing(context.query, loadStore(), {
-//     [Loads.TOKEN_FROM_SERVER]: (dispatch) => {
-//       dispatch(
-//         updateUserInfo({
-//           name: "Prerender by Server",
-//           token: "example-token-from-sever",
-//         })
-//       );
+const styleBgLoginPanel = css`
+  background: url("/imgs/login-bg-1.jpeg") rgba(21, 41, 53, 0.9);
+  background-size: cover;
+  background-blend-mode: multiply;
 
-//       return ["session"];
-//     },
-//   });
+  & .bx--label {
+    @apply text-white;
+  }
+`;
 
-//   return {
-//     props: {
-//       ...initialProps,
-//     },
-//   };
-// };
+const styleBgEmptyPanel = css`
+  background: url("/imgs/login-bg-2.png");
+  @apply bg-contain bg-no-repeat w-1/2;
+`;
+
+const styleFullHeight = css`
+  height: 100vh;
+  background: url("/imgs/login-bg-3.jpeg");
+  @apply bg-contain;
+`;
 
 export default Login;
